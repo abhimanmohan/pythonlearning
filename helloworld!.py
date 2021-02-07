2ea79a3926b46381b0e94fa5c665e0ead66be18d
abhimanmohan <abhimanmohan@users.noreply.github.com>
2021-02-07 15:59:03 +0530

    Update helloworld!.py

--- helloworld!.py
+++ helloworld!.py
@@ -2,3 +2,27 @@
 print("'Hello'")
 print("the end","keep learning",44)
 
+great="ram"
+warrior="hanuman"
+print(great+" "+warrior)
+ala=32
+r="kunal"
+#print("name" +"is "+r +"my age is "+ala)
+a=12
+b=3
+for i in range(1, a/b):
+    print(i)
+"""
+print("we win")
+
+print("w\ne\n\n\nw\ni\nn\n")
+parrot="Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[1])
+print(parrot[2])
+print(parrot[3])
+print(parrot[4])
+print(parrot[6])
+print(parrot[7])
+print(parrot[8])
